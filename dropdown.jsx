19dcfe471b75eeb6af780bba506264843603db30
Safiya Cain <say.cain3@gmail.com>
2021-05-13 08:24:23 -0400

    redering in dropItem class

--- dropdown.jsx
+++ dropdown.jsx
@@ -1,10 +1,10 @@
-/* 
+/*
 
 Prompt:
   We have defined a basic dropdown via the Dropdown and DropdownItem components below, with example usage
-  in the ExampleNav component. The Dropdown and DropdownItem components have some problems, and also 
+  in the ExampleNav component. The Dropdown and DropdownItem components have some problems, and also
   have room for improvements (doesn't everything?) A couple items TODO here (make sure to explain with comments!)
-  
+
   0. How are you today? 😊
   1. Please fix any obvious issues you see with the dropdown.
   2. Please then make improvements to the dropdown.
@@ -14,7 +14,7 @@ Prompt:
      app.sync('PATCH', 'user', { dropdown_1_state: {true,false} }) where would this be included?
   5. If we wanted to pass children (like this example) OR a Promise that resolves to an array of items
      what changes should be made? (just a sentence or two or some code is ok).
-  
+
   PS: No need to worry about CSS.
 
  */
@@ -54,6 +54,7 @@ class Dropdown extends PureComponent {
 class DropdownItem extends PureComponent {
   render() {
     // TODO implement me
+    <h1>Hello Wordld</h1>
   }
 }
 
